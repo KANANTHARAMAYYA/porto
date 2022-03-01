3adc9b42f05e73c023378c082bb89c1934abc9c2
ARK <a7n007@gmail.com>
2022-03-01 01:36:15 -0700

    wat

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import * as THREE from 'three';
+import * as gsap from 'gsap';
+import { gsap, ScrollTrigger, Draggable, MotionPathPlugin, Flip } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger, Draggable, Flip, MotionPathPlugin); 
